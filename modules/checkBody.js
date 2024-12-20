c8e81bfefa90444b0471621126955bb853b46649
Ousmane <diarraousmane37@gmail.com>
2024-12-20 16:30:09 +0100

    modules de verifications de regex

---
+++ modules/checkBody.js
@@ -0,0 +1,19 @@
+/**
+ * Check if body contains all fields
+ * @param {Object} body {firstname:"john", lastname:"doe"}
+ * @param {Array} keys ["firstname", "lastname"]
+ * @returns true||false
+ */
+function checkBody(body, keys) {
+  let isValid = true;
+
+  for (const field of keys) {
+    if (!body[field] || body[field] === "") {
+      isValid = false;
+    }
+  }
+
+  return isValid;
+}
+
+module.exports = { checkBody };
---
+++ modules/checkFieldRegex.js
@@ -0,0 +1,39 @@
+/**
+ * Check if date format is valid
+ * @param {String} dateString 2024-12-20
+ * @returns true||false
+ */
+function isValidDateFormat(dateString) {
+  const regex = /^\d{4}-\d{2}-\d{2}$/;
+  return dateString.match(regex) !== null;
+}
+
+/**
+ * Check if email format is valid
+ * @param {String} email monemail@domaine.com
+ * @returns true||false
+ */
+const isValidEmail = (email) => {
+  const emailRegexValidation = /^\S+@\S+\.\S+$/;
+  if (!emailRegexValidation.test(email)) {
+    return false;
+  }
+  return true;
+};
+
+/**
+ * Check if password format is valid according to one of rgpd rules
+ * @param {String} password Mypassword123?
+ * @returns true||false
+ */
+const isValidPassword = (password) => {
+  const passwordRegexValidation =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  if (!passwordRegexValidation.test(password)) {
+    return false;
+  }
+  return true;
+};
+
+module.exports = { isValidDateFormat, isValidEmail, isValidPassword };

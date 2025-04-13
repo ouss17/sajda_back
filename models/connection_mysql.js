d3212fd03c874abce073392e6ac8a90fabf23d30
Ousmane Diarra <mr.ousmanediarra@gmail.com>
2025-04-14 01:46:16 +0200

    add test connection

--- models/connection_mysql.js
+++ models/connection_mysql.js
@@ -6,11 +6,11 @@ let pool;
 
 const createPool = () => {
   pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-    port: process.env.MYSQL_PORT,
+    host: process.env.MYSQL_HOST || "localhost",
+    user: process.env.MYSQL_USER || 'test',
+    password: process.env.MYSQL_PASSWORD || "testJestOrJunit",
+    database: process.env.MYSQL_DB || "sajda",
+    port: process.env.MYSQL_PORT || "3307",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
